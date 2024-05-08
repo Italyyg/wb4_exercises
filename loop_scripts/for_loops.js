a1a80aa72ac7cafad0271c027a319c991f7858a2
Italy Gonzalez <italygonzalez2017@yahoo.com>
2024-05-08 14:11:39 -0500

    added correct solution for product code and added loops file

---
+++ loop_scripts/for_loops.js
@@ -0,0 +1,13 @@
+"use strict"
+
+for (let i = 0; i <= 6; i++) {
+    console.log("I love loops")
+
+}
+
+let i =0;
+while(i<=6){
+    console.log("I love loops")
+    i++
+
+}
--- product_code.js
+++ product_code.js
@@ -1,20 +1,38 @@
 "use strict"
 
-let partCode={
 
-    supplierCode:"SZA",
-    productNumber:"2987",
-    size:"M"
-
-
-
-}
-
+//function to parse code
 function parsePartCode(code){
 
-    console.log(`
-    ${code.supplierCode}:${code.productNumber}-${code.size} `)
+
+    //get the supplier code from the passed in string
+    let supplierCode = code.substring(0, code.indexOf(":"));
+
+    //get the product number from the passed in string
+    let productNumber = code.substring(code.indexOf(":")+1, code.indexOf("-"));
+
+    //get the size from the passed in string
+    let size = code.substring(code.indexOf("-")+1);
+
+    //put all that together in and object and return from the function
+    return {
+        supplierCode: supplierCode,
+        productNumber: productNumber,
+        size: size
+    }
 
 }
 
-parsePartCode(partCode)
+//create a variable holding some product code
+let potato = "XYZ:1234-L";
+
+//run the function and assing the return value to a variable
+let parsedCode = parsePartCode(potato);
+
+//work with the variable to diplay the information in the console
+console.log(`
+Supplier: ${parsedCode.supplierCode}
+Product Number: ${parsedCode.productNumber}
+Size: ${parsedCode.size}
+`);
+ 
